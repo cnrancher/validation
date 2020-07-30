af926fc8330282e33898b1904e52015daf179172
TangStone <18845599643@163.com>
2020-07-30 09:50:02 +0800

    jenkins调试

--- tests/v3_api/test_ack_tg_cluster.py
+++ tests/v3_api/test_ack_tg_cluster.py
@@ -3,8 +3,8 @@ import pystache as pystache
 from .entfunc import *  # NOQA
 import pytest
 
-ACK_ACCESS_KEY = os.environ.get('ALIYUN_ACCESS_KEY', "")
-ACK_SECRET_KEY = os.environ.get('ALIYUN_SECRET_KEY', "")
+ACK_ACCESS_KEY = os.environ.get('RANCHER_ALIYUN_ACCESS_KEY', "")
+ACK_SECRET_KEY = os.environ.get('RANCHER_ALIYUN_SECRET_KEY', "")
 
 
 ackcredential = pytest.mark.skipif(not (ACK_ACCESS_KEY and ACK_SECRET_KEY),
@@ -13,7 +13,7 @@ ackcredential = pytest.mark.skipif(not (ACK_ACCESS_KEY and ACK_SECRET_KEY),
 
 
 @ackcredential
-def test_create_ack_cluster():
+def test_create_ack_tg_cluster():
     client = get_admin_client()
     ackConfig = get_ack_config()
     print("Cluster creation")
--- tests/v3_api/test_ack_zy_cluster.py
+++ tests/v3_api/test_ack_zy_cluster.py
@@ -5,15 +5,15 @@ import pytest
 
 
 
-ACK_ACCESS_KEY = os.environ.get('ALIYUN_ACCESS_KEY', "")
-ACK_SECRET_KEY = os.environ.get('ALIYUN_SECRET_KEY', "")
+ACK_ACCESS_KEY = os.environ.get('RANCHER_ALIYUN_ACCESS_KEY', "")
+ACK_SECRET_KEY = os.environ.get('RANCHER_ALIYUN_SECRET_KEY', "")
 
 
 ackcredential = pytest.mark.skipif(not (ACK_ACCESS_KEY and ACK_SECRET_KEY),
                                    reason='ACK Credentials not provided, '
                                           'cannot create cluster')
 @ackcredential
-def test_create_ack_cluster():
+def test_create_ack_zy_cluster():
     client = get_admin_client()
     ackConfig = get_ack_config()
     print("Cluster creation")
